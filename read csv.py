5a0ed4eb0c048a039027ec53fbb17265da06e332
markmum0 <marckmumo@gmail.com>
2024-04-03 06:13:18 +0300

    4th commit

--- read csv.py
+++ read csv.py
@@ -1,12 +1,21 @@
 # first the import
 import csv
+from collections import Counter
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 # the method below implements the seaborn-v0_8-darkgrid style
 plt.style.use('ggplot')
+#opening the data.csv file
+with open('data.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+# using the counter function from collections to count the frequency of the languages used.
+    language_counter = Counter()
+# looping through the csv file
+    for row in csv_reader:
+        language_counter.update(row['LanguagesWorkedWith'].split(';'))
 
-
+print(language_counter.most_common(15))
 #plt.title('Salaries for developers')
 #plt.xlabel('Ages')
 #plt.ylabel('Salaries (USD)')
